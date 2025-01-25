b5c5fd509f6352dd0ed56a09ef73aa7e13114646
komacat <yeahdsj@gmail.com>
2025-01-25 00:42:49 -0800

    test

--- src/routes/songlist/+page.ts
+++ src/routes/songlist/+page.ts
@@ -1,7 +1,5 @@
+import data from "$lib/songlist/songs.json"
 
 export const load = async ({ fetch }) => {
-    const jsonURL = "/src/lib/songlist/songs.json"
-    const response = await fetch(jsonURL)
-    const data = await response.json()
     return { data }
 }
