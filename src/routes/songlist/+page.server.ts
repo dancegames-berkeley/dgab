2d92c2e23727bb8998445f049b46be152475f71e
komacat <yeahdsj@gmail.com>
2025-01-29 13:10:12 -0800

    update song path

--- src/routes/songlist/+page.server.ts
+++ src/routes/songlist/+page.server.ts
@@ -1,6 +1,6 @@
 export const load = async ({ fetch }) => {
     try {
-        const response = await fetch("http://localhost:3000/songs.json");
+        const response = await fetch("https://dancegames.studentorg.berkeley.edu/songs.json");
         const data = await response.json();
         return {
             data
