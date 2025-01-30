5c3db01a0f8de835552ceb4fe7764be96923adef
komacat <yeahdsj@gmail.com>
2025-01-30 15:52:15 -0800

    use ocf server for songs json

--- src/routes/songlist/+page.server.ts
+++ src/routes/songlist/+page.server.ts
@@ -2,7 +2,7 @@ const s3Bucket = import.meta.env.VITE_S3_BUCKET;
 export const load = async ({ fetch }) => {
     try {
         console.log(s3Bucket + 'songs.json');
-        const response = await fetch(s3Bucket + 'songs.json');
+        const response = await fetch('https://dancegames.studentorg.berkeley.edu/' + 'songs.json');
         if (!response.ok) {
             throw new Error(`HTTP error: ${response.status}`);
         }
