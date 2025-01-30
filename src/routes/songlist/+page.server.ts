2ae862e99e238d12f652af5cc29fcc527debe166
komacat <yeahdsj@gmail.com>
2025-01-30 15:47:49 -0800

    add error checking

--- src/routes/songlist/+page.server.ts
+++ src/routes/songlist/+page.server.ts
@@ -3,6 +3,9 @@ export const load = async ({ fetch }) => {
     try {
         console.log(s3Bucket + 'songs.json');
         const response = await fetch(s3Bucket + 'songs.json');
+        if (!response.ok) {
+            throw new Error(`HTTP error: ${response.status}`);
+        }
         const data = await response.json();
         return {
             data
