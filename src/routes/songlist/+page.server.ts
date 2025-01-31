81d02bdb07acae763948b745241108012ec27508
komacat <yeahdsj@gmail.com>
2025-01-31 11:48:30 -0800

    use dynamically served json for songs list :(

--- src/routes/songlist/+page.server.ts
+++ src/routes/songlist/+page.server.ts
@@ -2,7 +2,7 @@
 export const load = async ({ fetch }) => {
     try {
         const s3Bucket = "https://dancegames.s3.us-west-1.amazonaws.com/";
-        const response = await fetch(s3Bucket + 'songs.json');
+        const response = await fetch('https://dancegames.studentorg.berkeley.edu/' + 'songs.json');
         if (!response.ok) {
             throw new Error(`HTTP error: ${response.status}`);
         }
