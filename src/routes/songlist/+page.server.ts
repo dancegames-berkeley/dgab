590113128f0d010f249d4cb0b9be80bafab38e34
komacat <yeahdsj@gmail.com>
2025-02-05 16:34:05 -0800

    use memcached for songs

--- src/routes/songlist/+page.server.ts
+++ src/routes/songlist/+page.server.ts
@@ -1,15 +1,37 @@
+import Memcached from 'memcached';
+const client = new Memcached('https://dancegames.studentorg.berkeley.edu/');
 
 export const load = async ({ fetch, setHeaders }) => {
-    setHeaders({ 'cache-control': 'public, max-age=86400' }); // cache for 1 day
+    setHeaders({ 'cache-control': 'public, max-age=3600' }); // cache client-side for 1 hour
+    client.get('songs', (error: Error, value: any) => {
+        if (error) {
+            console.error(error);
+        } else {
+            if (value === null) {
+                throw new Error('No value found in cache');
+            }
+            const data = JSON.parse(value);
+            console.log(data);
+            return { data };
+        }
+    });
+    client.end();
     try {
         const response = await fetch('https://dancegames.studentorg.berkeley.edu/' + 'songs.json');
         if (!response.ok) {
             throw new Error(`HTTP error: ${response.status}`);
         }
         const data = await response.json();
-        return {
-            data
-        };
+        // cache server-side for 1 day
+        client.set('songs', JSON.stringify(data), 86400, (error: Error, result: Boolean) => {
+            if (error) {
+                console.error(error);
+            } else {
+                console.log(result);
+            }
+        });
+        client.end();
+        return { data };
     } catch (error) {
         throw new Error(`Error: ${error}`);
     }
--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 import { handler } from '../build/handler.js';
-import fs from 'fs';
 import express from 'express';
 
 const assetsPath = './data/assets';
 
 const app = express();
+app.use(express.json());
 
 // serve assets dynamically
 app.use(express.static(assetsPath))
